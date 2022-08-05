56dd39930b3cda6b0fc9a0c5c3eadfadb1ec53cd
Johannes Lampel <johannes.lampel@microsoft.com>
2022-08-05 13:18:46 +0200

    Added config folder

--- src/ContributorLicenseAgreement.Core/Primitives/Data/MinimalChangeRequired.cs
+++ src/ContributorLicenseAgreement.Core/Primitives/Data/MinimalChangeRequired.cs
@@ -1,7 +1,7 @@
-﻿// /*---------------------------------------------------------------------------------------------
-//  *  Copyright (c) Microsoft Corporation. All rights reserved.
-//  *  Licensed under the Microsoft License. See License.txt in the project root for license information.
-//  *--------------------------------------------------------------------------------------------*/
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Microsoft License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
 namespace ContributorLicenseAgreement.Core.Primitives.Data
 {
