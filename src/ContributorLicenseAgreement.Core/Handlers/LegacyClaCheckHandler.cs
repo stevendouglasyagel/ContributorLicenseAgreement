e5fe05e1852b3e18d6b7a16343d8825e94f1396f
Johannes Lampel <johannes.lampel@microsoft.com>
2022-09-01 14:39:50 +0200

    Unused imports removed

--- src/ContributorLicenseAgreement.Core/Handlers/LegacyClaCheckHandler.cs
+++ src/ContributorLicenseAgreement.Core/Handlers/LegacyClaCheckHandler.cs
@@ -5,7 +5,6 @@
 
 namespace ContributorLicenseAgreement.Core.Handlers
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Net.Http;
@@ -17,9 +16,7 @@ namespace ContributorLicenseAgreement.Core.Handlers
     using GitOps.Clients.Azure.Telemetry;
     using GitOps.Clients.GitHub;
     using GitOps.Clients.GitHub.Configuration;
-    using GitOps.Clients.GitHub.Models;
     using Microsoft.Extensions.Logging;
-    using Octokit;
     using CommitState = GitOps.Abstractions.CommitStatus.CommitState;
     using CommitStatus = GitOps.Clients.GitHub.Models.CommitStatus;
 
